bed9211d04c99f20c38c5f656815c779bc258af6
aridane <aridanecabreraramirez@gmail.com>
2023-11-17 18:59:14 +0100

    Fresh_Install

---
+++ app/Models/Modulo.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Modulo extends Migration
+{
+    public function up()
+    {
+        Schema::create('modulos', function (Blueprint $table) {
+            $table->id();
+            $table->string('codigo');
+            $table->string('materia');
+            $table->integer('h_semanales');
+            $table->integer('h_totales');
+            $table->enum('turno', ['mañana', 'tarde']);
+            $table->string('aula');
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('especialidad_id');
+            $table->unsignedBigInteger('estudio_id');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('especialidad_id')->references('id')->on('especialidades');
+            $table->foreign('estudio_id')->references('id')->on('estudios');
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('modulos');
+    }
+}
